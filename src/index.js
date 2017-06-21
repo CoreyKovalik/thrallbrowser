9dcf40c61469beeb8af97b0c4e1d4e0d590e9853
Immotal <coreykovalik@gmail.com>
2017-06-20 20:21:06 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import $ from "jquery";
+
+console.log("Hello World");
+
+var element = document.createElement('div');
+
+element.innerHTML = '<b>Hello World</b>';
+
+document.body.appendChild(element);
