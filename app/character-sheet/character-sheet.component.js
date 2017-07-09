50adde65baa19fb08b0dbf3882072b1505b47c55
Immotal <coreykovalik@gmail.com>
2017-07-09 02:56:54 -0400

    add content to character-sheet template

--- app/character-sheet/character-sheet.component.js
+++ app/character-sheet/character-sheet.component.js
@@ -2,13 +2,14 @@ angular
   .module('thrallbro')
   .component('characterSheet', {
     templateUrl: 'character-sheet/character-sheet.template.html',
+    controllerAs: 'the',
     controller: function characterSheetController($http, $routeParams){
       var self = this;
 
       function loadCharacterData() {
         self.isloading = true;
-        $http.get('https://serverthrallapi.herokuapp.com/api/5/characters/' + $routeParams.charID + '?private_secret=200cd768-5b1d-11e7-9e82-d60626067254').then(function(response) {
-            self.characters = response.data;
+        $http.get('https://serverthrallapi.herokuapp.com/api/5/characters/' + $routeParams.charId + '?private_secret=200cd768-5b1d-11e7-9e82-d60626067254').then(function(response) {
+            self.char = response.data;
             self.isloading = false;
         });
       }
