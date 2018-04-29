3c99634193b874bdffa3b19511684d23d6228e15
Immotal <coreykovalik@gmail.com>
2018-04-29 17:47:31 -0400

    load current server data with name into navbar

--- app/components/servernavbar/servernavbar.component.js
+++ app/components/servernavbar/servernavbar.component.js
@@ -3,9 +3,18 @@ angular
   .component('serverNavbar', {
     templateUrl: 'components/servernavbar/servernavbar.template.html',
     controllerAs: 'navbarCtrl',
-    controller: function serverController($q, $route, $routeParams) {
+    controller: function serverController($q, $route, $routeParams, serverthrallapi) {
       var self = this;
       self.routeId = $route.current.$$route.id;
       self.serverId = $routeParams.serverId;
+
+      function loadServer() {
+        serverthrallapi.getServer(self.serverId)
+          .then(function(server) {
+            self.server = server;
+          });
+      }
+
+      loadServer();
     }
   });
