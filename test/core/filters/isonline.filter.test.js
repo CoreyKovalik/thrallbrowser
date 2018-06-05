71a7681259e46372e62aeefb06219339e6141d95
David Stensland <me@terite.com>
2018-06-04 22:38:45 -0700

    write an initial test

---
+++ test/core/filters/isonline.filter.test.js
@@ -0,0 +1,27 @@
+'use strict';
+
+describe('Filter: isCharacterOnline', function() {
+  let isCharacterOnline;
+
+  beforeEach(module('thrallbrowser'));
+
+  beforeEach(inject(function (_isCharacterOnlineFilter_) {
+    isCharacterOnline = _isCharacterOnlineFilter_;
+  }));
+
+  it('should handle "Online Now"', function () {
+    let character = {
+      is_online: true,
+      last_online: 0
+    };
+    expect(isCharacterOnline(character)).toBe('Online Now');
+  });
+
+  it('should handle last online', function () {
+    let character = {
+      is_online: false,
+      last_online: 0
+    };
+    expect(isCharacterOnline(character)).toEndWith('years ago');
+  });
+});
